de6250c04f9777f450a504876799865fc8a9e362
Mykola Fant <Mykola.Fant@infopulse.com>
2020-10-02 16:36:59 +0300

    update(zipper): extend sonfig

--- modules/zipper.js
+++ modules/zipper.js
@@ -3,9 +3,22 @@ const archiver = require('archiver');
 const { ensurePath, pathExists } = require('./file_handler');
 
 /**
+ * @typedef {object} ZipperConfig
+ * @property {string} [directory] - if you want to create a ZIP from directory
+ * @property {string} [zipName] - name of the ZIP you want to create, ignored if `outputStream === true`
+ * @property {string} [dest] - dest of the created ZIP, ignored if `outputStream === true`
+ * @property {string} [directory] - if you want to create a zip from directory
+ * @property {ReadableStream|ReadableStream[]} - streamor array of streams to be read from
+ * @property {string} [glob] - e.g. if you want a directory with some excluded files you may pass src/**\/!(banana.txt)
+ *                                  or to exclude a subdirectrory src/!(bananas)/**\/*
+ *                                  Note: "\" is used for escaping comment end in the examples above
+ * @property {boolean} [outputStream=false] - by default the handler will create a file in the fs and resolve the path
+ */
+
+/**
 * Creates a zip file from source
 * common-mods/modules/zipper
-* @param {String} source
+* @param {String|ZipperConfig} source
 * @param {String} zipName
 * @param {String} dest
 * @param {Object} options
@@ -15,6 +28,10 @@ const { ensurePath, pathExists } = require('./file_handler');
 
 // TODO: add 'exclude' parameter
 module.exports.zipper = async (source, zipName, dest) => new Promise(async (resolve, reject) => {
+  const config = typeof source === 'string'
+    ? { directory: source }
+    : { ...source };
+
   if (pathExists(source)) {
     const nameWithDotZip = zipName.endsWith('.zip') ? zipName : `${zipName}.zip`;
     const zipPath = `${dest}/${nameWithDotZip}`;
